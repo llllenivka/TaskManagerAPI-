8d6d6ccf5b42ee446728bbae4f5add886bae4fd1
Adelina Sharafutdinova <a.sharafutdinovva@gmail.com>
2025-02-03 17:42:58 +0300

    connect db

--- src/TaskManager/Program.cs
+++ src/TaskManager/Program.cs
@@ -1,6 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using TaskManager.Data;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite("Data Source=tasks.db"));
+
+var app = builder.Build();
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+// app.MapGet("/", () => "Hello World!");
 
 app.Run();
