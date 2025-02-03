26dc8e43eb72c387dd5bebff4a9490c01e477a93
Adelinaa <adelinaa@MacBook-Pro-Adelinaa.local>
2025-02-03 17:24:00 +0300

    add: Model Task, connect db task

---
+++ src/TaskManager/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManager.Models;
+
+
+namespace TaskManager.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
+        public DbSet<TaskModel> Tasks { get; set; }
+
+    }
+
+}
+
